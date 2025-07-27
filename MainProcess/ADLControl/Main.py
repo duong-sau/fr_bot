1cc9fb99db510caeaa010c9ce7c80ae3231ecb42
duong-sau <duongsau1211@gmail.com>
2025-07-27 13:41:56 +0900

    fix adl bug

--- MainProcess/ADLControl/Main.py
+++ MainProcess/ADLControl/Main.py
@@ -1,4 +1,5 @@
 import asyncio
+import copy
 import os
 import sys
 import time
@@ -25,12 +26,19 @@ class ADLController:
 
         self.lock = asyncio.Lock()
         self.positions = {}
+        self.old_positions = {}
         self.error_count = 0
 
 
     def check_position_change(self, symbol):
-        bitget_total, bitget_side, bitget_contract_size = fetch_position_bitget(self.bitget_exchange, symbol)
-        gate_total, gate_side, gate_contract_size = fetch_position_gate(self.gate_exchange, symbol)
+        bitget_total, bitget_side, bitget_contract_size = try_this(fetch_position_bitget,
+                                                                   params={'bitget_exchange': self.bitget_exchange,
+                                                                           'symbol': symbol},
+                                                                   log_func=adl_log, retries=5, delay=1)
+        gate_total, gate_side, gate_contract_size = try_this(fetch_position_gate,
+                                                            params={'gate_exchange': self.gate_exchange,
+                                                                    'symbol': symbol},
+                                                            log_func=adl_log, retries=5, delay=1)
 
         if gate_total == 0 and bitget_total == 0:
             return
@@ -41,12 +49,38 @@ class ADLController:
             diff = gate_total - bitget_total
             adl_log(f"Gate has more position: {diff} {symbol}")
             diff_contras = diff / gate_contract_size
-            close_position_gate(self.gate_exchange, symbol, gate_side, diff_contras)
+            try:
+                close_position_gate(self.gate_exchange, symbol, gate_side, diff_contras)
+                adl_log(f"Closed position on GateIO: {symbol}, Size: {diff_contras} {gate_side}")
+            except Exception as e:
+                print(e)
+                adl_log(f"Error closing position on GateIO: {e}")
         elif bitget_total > gate_total:
             diff = bitget_total - gate_total
             adl_log(f"Bitget has more position: {diff} {symbol}")
             diff_contras = diff / bitget_contract_size
-            close_position_bitget(self.bitget_exchange, symbol, bitget_side, diff_contras)
+            try:
+                close_position_bitget(self.bitget_exchange, symbol, bitget_side, diff_contras)
+                adl_log(f"Closed position on Bitget: {symbol}, Size: {diff_contras} {bitget_side}")
+            except Exception as e:
+                print(e)
+                adl_log(f"Error closing position on Bitget: {e}")
+
+    def check_position_change_by_ws(self):
+        for p_symbol in self.positions.keys():
+            if p_symbol not in self.old_positions.keys():
+                adl_log(f"Position for {p_symbol} has been removed")
+                continue
+
+            old_bitget_size = self.old_positions[p_symbol].get('bitget_size', 0)
+            old_gate_size = self.old_positions[p_symbol].get('gate_size', 0)
+
+            new_bitget_size = self.positions[p_symbol].get('bitget_size', 0)
+            new_gate_size = self.positions[p_symbol].get('gate_size', 0)
+
+            if old_bitget_size != new_bitget_size or old_gate_size != new_gate_size:
+                adl_log(f"Position changed for {p_symbol}: Bitget {old_bitget_size} -> {new_bitget_size}, GateIO {old_gate_size} -> {new_gate_size}")
+                self.check_position_change(p_symbol)
 
     async def sync_hedge(self, exchange, symbols):
         await exchange.load_markets()
@@ -59,98 +93,30 @@ class ADLController:
                 pos = await exchange.watch_positions(symbols=symbols)
                 print(pos)
 
+                self.old_positions = copy.deepcopy(self.positions)
                 # Check for symbols not present in current positions
                 current_symbols = {p['symbol'] for p in pos}
-                for symbol in symbols:
+                for symbol in self.positions.keys():
                     if symbol not in current_symbols:
-                        async with self.lock:
-                            old_bitget_size = self.positions.get(symbol, {}).get('bitget_size', 0)
-                            old_gate_size = self.positions.get(symbol, {}).get('gate_size', 0)
-                        if exchange.id == 'bitget' and old_bitget_size != 0:
-                            adl_log(f"Bitget position changed for {symbol}: {old_bitget_size} -> 0")
-                            try_this(self.check_position_change, params={'symbol': symbol}, log_func=adl_log, retries=5,
-                                     delay=1)
-                            async with self.lock:
-                                self.positions.setdefault(symbol, {})['bitget_size'] = 0
-                        elif exchange.id == 'gateio' and old_gate_size != 0:
-                            adl_log(f"GateIO position changed for {symbol}: {old_gate_size} -> 0")
-                            try_this(self.check_position_change, params={'symbol': symbol}, log_func=adl_log, retries=5,
-                                     delay=1)
-                            async with self.lock:
-                                self.positions.setdefault(symbol, {})['gate_size'] = 0
+                        if exchange.id == 'bitget':
+                            adl_log(f"Bitget position for {symbol} is be size 0")
+                            self.positions[symbol]['bitget_size'] = 0
+                        elif exchange.id == 'gateio':
+                            adl_log(f"GateIO position for {symbol} is be size 0")
+                            self.positions[symbol]['gate_size'] = 0
 
                 for p in pos:
                     p_symbol = p['symbol']
                     p_size = float(p['contracts']) * float(p['contractSize'])
 
                     async with self.lock:
-                        if p_symbol not in self.positions:
-                            self.positions[p_symbol] = {
-                                'gate_size': p_size,
-                                'bitget_size': p_size,
-                            }
-                    old_bitget_size = self.positions[p_symbol]['bitget_size']
-                    old_gate_size = self.positions[p_symbol]['gate_size']
-
-                    async with self.lock:
                         if exchange.id == 'bitget':
-                            self.positions[p_symbol]['bitget_size'] = p_size
+                            self.positions.setdefault(p_symbol, {})['bitget_size'] = p_size
                         elif exchange.id == 'gateio':
-                            self.positions[p_symbol]['gate_size'] = p_size
-                        else:
-                            adl_log(f"Unknown exchange id: {exchange.id}")
-                            sys.exit(1)
+                            self.positions.setdefault(p_symbol, {})['gate_size'] = p_size
 
-                    if exchange.id == 'bitget':
-                        if old_bitget_size != p_size:
-                            adl_log(f"Bitget position changed for {p_symbol}: {old_bitget_size} -> {p_size}")
-                            try_this(self.check_position_change, params={'symbol': p_symbol}, log_func=adl_log,
-                                     retries=5, delay=1)
-                        self.positions[p_symbol]['bitget_size'] = p_size
-                    elif exchange.id == 'gateio':
-                        if old_gate_size != p_size:
-                            adl_log(f"GateIO position changed for {p_symbol}: {old_gate_size} -> {p_size}")
-                            try_this(self.check_position_change, params={'symbol': p_symbol}, log_func=adl_log,
-                                     retries=5, delay=1)
-                    else:
-                        adl_log(f"Unknown exchange id: {exchange.id}")
-                        sys.exit(1)
-                self.error_count = self.error_count - 1 if self.error_count > 1 else 0
-
-                for p in pos:
-                    p_symbol = p['symbol']
-                    p_size = float(p['contracts']) * float(p['contractSize'])
-
-                    async with self.lock:
-                        if p_symbol not in self.positions:
-                            self.positions[p_symbol] = {
-                                'gate_size': p_size,
-                                'bitget_size': p_size,
-                            }
-                    old_bitget_size = self.positions[p_symbol]['bitget_size']
-                    old_gate_size = self.positions[p_symbol]['gate_size']
-
-                    async with self.lock:
-                        if exchange.id == 'bitget':
-                            self.positions[p_symbol]['bitget_size'] = p_size
-                        elif exchange.id == 'gateio':
-                            self.positions[p_symbol]['gate_size'] = p_size
-                        else:
-                            adl_log(f"Unknown exchange id: {exchange.id}")
-                            sys.exit(1)
-
-                    if exchange.id == 'bitget':
-                        if old_bitget_size != p_size:
-                            adl_log(f"Bitget position changed for {p_symbol}: {old_bitget_size} -> {p_size}")
-                            try_this(self.check_position_change, params={'symbol': p_symbol}, log_func=adl_log, retries=5, delay=1)
-                        self.positions[p_symbol]['bitget_size'] = p_size
-                    elif exchange.id == 'gateio':
-                        if old_gate_size != p_size:
-                            adl_log(f"GateIO position changed for {p_symbol}: {old_gate_size} -> {p_size}")
-                            try_this(self.check_position_change, params={'symbol': p_symbol}, log_func=adl_log, retries=5, delay=1)
-                    else:
-                        adl_log(f"Unknown exchange id: {exchange.id}")
-                        sys.exit(1)
+                # Check for changes in positions
+                self.check_position_change_by_ws()
                 self.error_count = self.error_count - 1 if self.error_count > 1 else 0
 
             except Exception as e:
